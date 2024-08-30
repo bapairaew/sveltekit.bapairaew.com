370f58473bf8675916c51e4a0ef9411e8f3b959d
dom <iam@bapairaew.com>
2024-08-30 17:35:57 +0700

    update footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,5 +12,5 @@
 <RootNavigation />
 <slot />
 <footer class="flex text-neutral-500 justify-end container mx-auto p-4">
-	© {new Date().getFullYear()} Narudom Techaval
+	© {new Date().getFullYear()} Dom.
 </footer>
